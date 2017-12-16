accee1ed0c49cc1265b9972547a398956c141114
dasvootz <ericvoots@gmail.com>
2017-12-16 12:59:22 -0500

    Splitting up logs due to 32gb RAM filling up

--- user_log_data.py
+++ user_log_data.py
@@ -2,37 +2,59 @@ import pandas as pd
 import gc
 import numpy as np
 #create user log dataframe
-df_user = pd.read_csv('data\\user_logs.csv',nrows=1000000)
-df_user2 = pd.read_csv('data\\user_logs_v2.csv',nrows=100000)
-df_user_all = df_user.append(df_user2)
+df_user = pd.read_csv('data\\user_logs.csv',nrows=10000)
+
+#split user logs up then combine, dont combine due to RAM limitations
+
 print('user length\n', len(df_user))
-print('\nuser length 2\n', len(df_user2))
-del df_user, df_user2
-gc.collect()
-print('\nuserlength all\n', len(df_user_all))
 
-#lagged changes
-df_user_all['num_25_l1'] = df_user_all.groupby('msno')['num_25'].shift(1)
-df_user_all['num_50_l1'] = df_user_all.groupby('msno')['num_50'].shift(1)
-df_user_all['num_75_l1'] = df_user_all.groupby('msno')['num_75'].shift(1)
-df_user_all['num_985_l1'] = df_user_all.groupby('msno')['num_985'].shift(1)
-df_user_all['num_100_l1'] = df_user_all.groupby('msno')['num_100'].shift(1)
-df_user_all['total_secs_l1'] = df_user_all.groupby('msno')['total_secs'].shift(1)
-df_user_all = df_user_all.fillna(0)
+
+
+#lagged changes for first log file
+df_user['num_25_l1'] = df_user.groupby('msno')['num_25'].shift(1)
+df_user['num_50_l1'] = df_user.groupby('msno')['num_50'].shift(1)
+df_user['num_75_l1'] = df_user.groupby('msno')['num_75'].shift(1)
+df_user['num_985_l1'] = df_user.groupby('msno')['num_985'].shift(1)
+df_user['num_100_l1'] = df_user.groupby('msno')['num_100'].shift(1)
+df_user['total_secs_l1'] = df_user.groupby('msno')['total_secs'].shift(1)
+df_user = df_user.fillna(0)
+print('finished lagged variables')
+
 #percentage changes
-df_user_all['num_25_pct'] = (df_user_all['num_25'] - df_user_all['num_25_l1']) / (df_user_all['num_25_l1'])
-df_user_all['num_50_pct'] = (df_user_all['num_50'] - df_user_all['num_50_l1']) / (df_user_all['num_50_l1'])
-df_user_all['num_75_pct'] = (df_user_all['num_75'] - df_user_all['num_75_l1']) / (df_user_all['num_75_l1'])
-df_user_all['num_985_pct'] = (df_user_all['num_985'] - df_user_all['num_985_l1']) / (df_user_all['num_985_l1'])
-df_user_all['num_100_pct'] = (df_user_all['num_100'] - df_user_all['num_100_l1']) / (df_user_all['num_100_l1'])
-df_user_all['total_secs_pct'] = (df_user_all['total_secs'] - df_user_all['total_secs_l1']) / (df_user_all['total_secs_l1'])
+df_user['num_25_pct'] = (df_user['num_25'] - df_user['num_25_l1']) / (df_user['num_25_l1'])
+df_user = df_user.drop(['num_25_l1'], axis=1)
+gc.collect()
+df_user['num_50_pct'] = (df_user['num_50'] - df_user['num_50_l1']) / (df_user['num_50_l1'])
+df_user = df_user.drop(['num_50_l1'], axis=1)
+gc.collect()
+df_user['num_75_pct'] = (df_user['num_75'] - df_user['num_75_l1']) / (df_user['num_75_l1'])
+df_user = df_user.drop(['num_75_l1'], axis=1)
+gc.collect()
+df_user['num_985_pct'] = (df_user['num_985'] - df_user['num_985_l1']) / (df_user['num_985_l1'])
+df_user = df_user.drop(['num_985_l1'], axis=1)
+gc.collect()
+df_user['num_100_pct'] = (df_user['num_100'] - df_user['num_100_l1']) / (df_user['num_100_l1'])
+df_user = df_user.drop(['num_100_l1'], axis=1)
+gc.collect()
+df_user['total_secs_pct'] = (df_user['total_secs'] - df_user['total_secs_l1']) / (df_user['total_secs_l1'])
+df_user = df_user.drop(['total_secs_l1'], axis=1)
+gc.collect()
+df_user = df_user.fillna(0)
+df_user = df_user.replace(np.inf, 0)
+df_user = df_user.replace(-np.inf, 0)
+print('finished pct variables')
 
-df_user_all= df_user_all.fillna(0)
-df_user_all = df_user_all.replace(np.inf, 0)
-df_user_all = df_user_all.replace(-np.inf, 0)
 
-#decile rank number of unique views
-df_user_all['unique_rank'] = pd.qcut(df_user_all['num_unq'], 10, labels=False)
+#indicator of last user log
+df_user.sort_values(['msno','date'], ascending=[True, False])
+df_user['last_user'] = (df_user.msno != df_user.msno.shift(-1)).astype(int)
+df_user = df_user[df_user.last_user == 1]
+gc.collect()
+
+print(df_user.head(5))
+
+#second log
+df_user2 = pd.read_csv('data\\user_logs_v2.csv',nrows=10000)
 
 
-print(df_user_all.head(10))
+print('Done')
